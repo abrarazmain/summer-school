263f1f20be3575dccc835cfeb9e2a22728e73504
abrarazmain <ajmainabrar5@gmail.com>
2023-06-15 16:42:15 +0600

    react router setup

---
+++ src/layOut/Main.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from "../Shared/Navbar";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </>
+  );
+};
+
+export default Main;
